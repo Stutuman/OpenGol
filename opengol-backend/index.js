39c88e0606ae4fe63f83f2c67899e8152f5b80f0
stutuman <la12cabj124@hotmail.com>
2026-02-25 03:20:06 -0300

    carpeta .env creada y puente entre node y la base de datos pgadming4

--- opengol-backend/index.js
+++ opengol-backend/index.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
 const express = require('express');
+const {Pool} = require('pg');
 const app = express();
 const puerto = 3000;
 
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+})
+pool.connect()
+    .then(() => console.log('¡Conexión exitosa a la base de datos de openGol! ⚽'))
+    .catch(err => console.error('Error conectando a la base de datos:', err.stack));
+    
 app.get('/', (req, res) => {
     res.send('¡El servidor de openGol está vivo y funcionando perfectamente!');
 });
